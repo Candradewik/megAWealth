faec00ea0776b450845ab463d4a3a19d428f260c
Candradewik <candradewi583@gmail.com>
2022-07-15 20:46:57 +0700

    update models

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Cart extends Model
 {
     use HasFactory;
+    public function realestates(){
+        return $this->hasMany(RealEstate::class);
+    }
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
 }

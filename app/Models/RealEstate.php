c15a29e361e6765e412dd42b661beb7845345146
Candradewik <candradewi583@gmail.com>
2022-07-15 21:14:58 +0700

    rename

--- app/Models/RealEstate.php
+++ app/Models/RealEstate.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class RealEstate extends Model
+class Realestate extends Model
 {
     use HasFactory;
     public function carts(){
